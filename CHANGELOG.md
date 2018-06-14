a9c5ac37534fe96439407c55d700a12771ef2dd5
hieunba <hi3unb@gmail.com>
2018-06-14 23:46:06 +0700

    Release 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# postman CHANGELOG
+
+This file listed CHANGELOG for postman cookbook.
+
+## 0.1.0 (2018-06-14)
+
+- Install postman from pre-built binary.
--- metadata.rb
+++ metadata.rb
@@ -7,14 +7,16 @@ long_description 'Installs/Configures postman'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
+supports 'ubuntu', '>= 16.04"
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/postman/issues'
+issues_url 'https://github.com/hieunba/chef-postman/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/postman'
+source_url 'https://github.com/hieunba/chef-postman'
